496206e1f53bb9af945267d5cbd0d2d9a63c37aa
GIMMIGDAL\alsanchezm <alsanchezm@gim.mx>
2022-11-07 22:02:52 -0600

    Implementación de apartado Web Implementación de LectorWebConfic

---
+++ SicossLectorWebConfig/VariablesWebConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SicossLectorWebConfig
+{
+    public static class VariablesWebConfig
+    {
+        public static string ServerURL => ConfigurationManager.AppSettings["ServerURL"];
+    }
+}
